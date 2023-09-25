4218e05c9d3d76bf83ef214fcee834235327055c
jieun2ya <143476495+jieun2ya@users.noreply.github.com>
2023-09-26 00:35:14 +0900

    [level 1] Title: 체육복, Time: 0.40 ms, Memory: 71.4 MB -BaekjoonHub

--- 프로그래머스/lv1/42862. 체육복/체육복.java
+++ 프로그래머스/lv1/42862. 체육복/체육복.java
@@ -2,33 +2,24 @@ import java.util.*;
 
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
-        int answer = n- lost.length;
+        int answer = n-lost.length;
         
-        List<Integer> lList = new ArrayList<>();
-    	for(int i : lost) lList.add(i);
-    	
-    	List<Integer> rList = new ArrayList<>();
-    	for(int i : reserve) rList.add(i);
-    	
-    	for (int i = 0; i < lList.size(); i++) {
-			for (int j = 0; j < rList.size(); j++) {
-				if(lList.get(i) == rList.get(j)) {
-					lList.remove(i); rList.remove(j);
-					n++; i--;
-					break;
+        for(int i = 0; i < lost.length; i++) {
+			if(Arrays.asList(reserve).contains(lost[i])) {
+				continue;
+			}
+			
+			for(int j: reserve) {
+				if(j == lost[i]-1 || j == lost[i]+1) {
+					answer++;
 				}
 			}
+			
 		}
-    	
-    	for (int i = 0; i < lList.size(); i++) {
-			for (int j = 0; j < rList.size(); j++) {
-				if(Math.abs(lList.get(i) - rList.get(j)) == 1) {
-					n++; rList.remove(j);
-					break;
-				}
-			}
+		
+		if(answer > n) {
+			answer = n;
 		}
-
         
         return answer;
     }
