97b1f14a9137ff959121b1338b90833820f28584
jieun2ya <143476495+jieun2ya@users.noreply.github.com>
2023-09-26 00:39:43 +0900

    Update 체육복.java

--- 프로그래머스/lv1/42862. 체육복/체육복.java
+++ 프로그래머스/lv1/42862. 체육복/체육복.java
@@ -4,29 +4,26 @@ class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
         int answer = n-lost.length;
         
-        Arrays.sort(lost);
-        Arrays.sort(reserve);
-        
-        // 여벌 체육복을 가져온 학생이 도난당한 경우
-        for(int i=0; i<lost.length; i++){
-            for(int j=0; j<reserve.length; j++){
-                if(lost[i] == reserve[j]){
-                    answer++;
-                    lost[i] = -1;
-                    reserve[j] = -1; 
-                    break; 
-                }
-            }
+       // 여벌 있지만 도둑맞아서 못빌려줌
+        for(int i=0; i<lost.length; i++) {
+        	for(int j=0; j<reserve.length; j++) {
+        		if(lost[i] == reserve[j]) {
+        			answer++;
+        			lost[i] = reserve[j] = -1;
+        			break;
+        		}
+        	}
         }
-        // 도난당한 학생에게 체육복 빌려주는 경우
-        for(int i=0; i<lost.length; i++){
-            for(int j=0; j<reserve.length; j++){
-                if((lost[i]-1 == reserve[j]) || (lost[i]+1 == reserve[j])){
-                    answer++;
-                    reserve[j] = -1; 
-                    break; 
-                }
-            }
+        
+        // 여벌없고 도둑맞음
+        for(int lostN : lost) {
+        	for(int i = 0; i<reserve.length; i++) {
+        		if(reserve[i] == lostN-1 || reserve[i] == lostN+1) {
+        			answer++;
+        			reserve[i] = -1;
+        			break;
+        		}
+        	}
         }
         
         return answer;
