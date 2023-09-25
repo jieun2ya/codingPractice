8babf04d2a703587e1d0030aae4284f7d023dab6
jieun2ya <143476495+jieun2ya@users.noreply.github.com>
2023-09-26 00:38:49 +0900

    Update 체육복.java

--- 프로그래머스/lv1/42862. 체육복/체육복.java
+++ 프로그래머스/lv1/42862. 체육복/체육복.java
@@ -4,22 +4,30 @@ class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
         int answer = n-lost.length;
         
-        for(int i = 0; i < lost.length; i++) {
-			if(Arrays.asList(reserve).contains(lost[i])) {
-				continue;
-			}
-			
-			for(int j: reserve) {
-				if(j == lost[i]-1 || j == lost[i]+1) {
-					answer++;
-				}
-			}
-			
-		}
-		
-		if(answer > n) {
-			answer = n;
-		}
+        Arrays.sort(lost);
+        Arrays.sort(reserve);
+        
+        // 여벌 체육복을 가져온 학생이 도난당한 경우
+        for(int i=0; i<lost.length; i++){
+            for(int j=0; j<reserve.length; j++){
+                if(lost[i] == reserve[j]){
+                    answer++;
+                    lost[i] = -1;
+                    reserve[j] = -1; 
+                    break; 
+                }
+            }
+        }
+        // 도난당한 학생에게 체육복 빌려주는 경우
+        for(int i=0; i<lost.length; i++){
+            for(int j=0; j<reserve.length; j++){
+                if((lost[i]-1 == reserve[j]) || (lost[i]+1 == reserve[j])){
+                    answer++;
+                    reserve[j] = -1; 
+                    break; 
+                }
+            }
+        }
         
         return answer;
     }
