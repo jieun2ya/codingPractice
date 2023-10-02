e0cd801e06d5f7f645387e2e5fe7989d274fb172
jieun2ya <143476495+jieun2ya@users.noreply.github.com>
2023-10-02 18:10:39 +0900

    [level 2] Title: 올바른 괄호, Time: 5.69 ms, Memory: 53.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,33 @@
+class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
+        int left = 0, right = 0;
+        int check = 0;
+        
+        for(int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+            
+            if(c == '('){
+                left++;
+                check++;
+            }else{
+                right++;
+                check--;
+            }
+            
+            if(check < 0){
+                answer = false;
+            }
+            
+        }
+        
+        if(left != right || s.charAt(0) == ')'){
+            answer = false;
+        }
+
+        
+        
+
+        return answer;
+    }
+}
