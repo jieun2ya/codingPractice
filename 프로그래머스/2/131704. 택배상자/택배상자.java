a65adfa20f3d3bf1b52d64fbfb06a455cb235b27
jieun2ya <143476495+jieun2ya@users.noreply.github.com>
2023-10-30 22:07:17 +0900

    [level 2] Title: 택배상자, Time: 1313.04 ms, Memory: 132 MB -BaekjoonHub

--- 프로그래머스/2/131704. 택배상자/택배상자.java
+++ 프로그래머스/2/131704. 택배상자/택배상자.java
@@ -2,30 +2,35 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] order) {
-      int answer = 0;
+        int answer = 0;
+        
+        int[] container = {5,4,3,2,1};
         Stack<Integer> containerSub = new Stack<>();
-        
         int originArr[] = order.clone();
-        
         Arrays.sort(order);
+
+        
+        //기사님이 원하는 상자 배열의 순서를 가리키는 변수
         int target = 0;
         
         for(int i = 0; i < originArr.length; i++){
-
+            //원하는 상자가 벨트에서 나오는 상자와 같으면
             if(order[i]==originArr[target]){
+            	// 상자싣고order, 다음 원하는 상자 찾기위해 ++
                 answer++;
                 target++;
-
-                int suppBeltSize = containerSub.size();
-                for(int j = 0; j < suppBeltSize; j++){
-
+                
+                //sub에 원하는 상자가 있는지 찾기위해 sub size()만큼 반복문 돌리기
+                int subSize = containerSub.size();
+                for(int j = 0; j < subSize; j++){
+                    //sub가 비어있지 않고 맨 앞에 있는 상자가 원하는 상자면 꺼내서 싣고, 다음원하는 상자 찾기위해++
                     if(!containerSub.isEmpty() && containerSub.peek()==originArr[target]){
                         answer++;
                         target++;
                         containerSub.pop();
                     }
                 }
-            }else{
+            }else{	// 원하는 상자가 벨트에서 나오는상자와 다르면 sub에 넣기
             	containerSub.push(order[i]);
             }
         }
