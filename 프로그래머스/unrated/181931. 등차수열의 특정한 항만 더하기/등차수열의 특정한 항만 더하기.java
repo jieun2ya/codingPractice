4b594d75c9914009514c9be79ccfbc2d40d52993
jieun2ya <143476495+jieun2ya@users.noreply.github.com>
2023-12-08 00:05:14 +0900

    [level unrated] Title: 등차수열의 특정한 항만 더하기, Time: 0.02 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/unrated/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.java
+++ 프로그래머스/unrated/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.java
@@ -2,9 +2,9 @@ class Solution {
     public int solution(int a, int d, boolean[] included) {
         int answer = 0;
         
-        for(int i=0; i < included.length; i++){
+        for(int i=0; i < included.length-1; i++){
             if(included[i]){
-                answer += a + (d*i);
+                answer += a + (d*i)
             }
         }
         return answer;
