2646d44b7d33e80ab52d62eeb9023c949b5cf52b
jieun2ya <143476495+jieun2ya@users.noreply.github.com>
2023-12-11 23:57:18 +0900

    [level unrated] Title: [PCCP 기출문제] 1번 / 붕대 감기, Time: 0.02 ms, Memory: 66.9 MB -BaekjoonHub

--- 프로그래머스/unrated/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.java
+++ 프로그래머스/unrated/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.java
@@ -1,34 +1,6 @@
 class Solution {
     public int solution(int[] bandage, int health, int[][] attacks) {
-        
-        int hp = health;
-        int time = 1;
-        int success = 0;
-        
-        for(int i=0; i<attacks.length; i++){
-            int checkTime = attacks[i][0] - time;
-            // 붕대감기로 체력 회복
-            hp += checkTime * bandage[1];
-            // 추가회복량 있는지 체크
-            hp += checkTime/bandage[0] * bandage[2];
-            // 최대 회복 넘게 회복된지 체크
-            if(hp > health){
-                hp = health;
-            }
-            // 공격 피해량만큼 피깎기
-            hp -= attacks[i][1];
-            if(hp <= 0){
-                break;
-            }
-            // 현재 시간 설정
-            time = attacks[i][0]+1;
-        }
-        int answer = hp;
-        if(hp <= 0){
-            answer = -1;
-        }
-        
-        
+        int answer = 0;
         return answer;
     }
 }
