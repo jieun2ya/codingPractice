a44701a49f68ac54a270fa5b8cbdfdc059170fa9
jieun2ya <143476495+jieun2ya@users.noreply.github.com>
2023-12-13 18:10:34 +0900

    Update ［PCCE 기출문제］ 10번 ／ 데이터 분석.java

--- 프로그래머스/unrated/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.java
+++ 프로그래머스/unrated/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.java
@@ -1,6 +1,45 @@
+import java.util.*;
+
 class Solution {
     public int[][] solution(int[][] data, String ext, int val_ext, String sort_by) {
-        int[][] answer = {};
+        //data에서 ext 값이 val_ext보다 작은 데이터만 뽑은 후, sort_by에 해당하는 값을 기준으로 오름차순으로 정렬
+        // 조건충족된것 넣어 놓을 list
+        List<int[]> check = new ArrayList<>();
+        // ["코드 번호(code)", "제조일(date)", "최대 수량(maximum)", "현재 수량(remain)"]
+        String[] str = {"code","date","maximum","remain"};
+        // 조건에 해당하는 idx,sortIdx 세팅
+        int idx = 0, sortIdx = 0;
+        for(int i=0; i<4; i++){
+            if(str[i].equals(ext)){
+                idx = i;
+            }
+            
+            if(str[i].equals(sort_by)){
+                sortIdx = i;
+            }
+        }
+        
+        
+        //data에서 ext 값이 val_ext보다 작은 데이터만 뽑은 후
+        for(int i=0; i<data.length; i++){
+            if(data[i][idx] < val_ext){
+                check.add(data[i]);
+            }
+        }
+        
+        int si = sortIdx;
+        // sort_by에 해당하는 값을 기준으로 오름차순으로 정렬
+        // 람다식에서 참조하는 외부 지역 변수는 final 혹은 effectively final 이어야한다
+        // https://vagabond95.me/posts/lambda-with-final/
+        Collections.sort(check,(o1,o2)->o1[si]-o2[si]);
+        
+        // 위에 정렬순서대로 값 넣어서 답 return
+        int[][] answer = new int[check.size()][4];
+        for(int i=0; i<check.size(); i++){
+            answer[i] = check.get(i);
+        }
+        
         return answer;
     }
+    
 }
