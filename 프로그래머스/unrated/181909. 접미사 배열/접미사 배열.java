7e5aaf8d48f2b57249b1f74b222ce4ec632b6edc
jieun2ya <143476495+jieun2ya@users.noreply.github.com>
2023-12-16 18:39:19 +0900

    [level unrated] Title: 접미사 배열, Time: 0.27 ms, Memory: 74 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181909. 접미사 배열/접미사 배열.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String my_string) {
+        
+        List<String> list = new ArrayList<>();
+        
+        for(int i=0; i<my_string.length(); i++){
+            list.add(my_string.substring(i));
+        }
+        
+        Collections.sort(list);
+        String[] answer = new String[list.size()];
+        for(int i=0; i<list.size(); i++){
+            answer[i] = list.get(i);
+        }
+        
+        return answer;
+    }
+}
